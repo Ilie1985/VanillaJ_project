e3a174db4e05639f6c81fd4c8c29f10e63ce5bad
Marian Ilie <ilie.marian32@yahoo.com>
2022-05-13 16:17:19 +0100

    email,pass length+pass match

--- form_validator/script.js
+++ form_validator/script.js
@@ -19,12 +19,14 @@ function showSuccess(input) {
 }
 
 //Cheeck if email is valid in javascript
-function isValid(email) {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+function checkEmail(input) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
 }
 //Get field name function
 //transform the first letter into upper case
@@ -43,6 +45,27 @@ const checkRequired = (inputArr) => {
     }
   });
 };
+
+// Check  input length function
+const checkLength = (input, min, max) => {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters `
+    );
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must have ${max} characters `);
+  } else {
+    showSuccess(input);
+  }
+};
+
+//Check password match function
+const checkPasswordsMatch= (input1,input2)=>{
+if (input1.value!==input2.value){
+  showError(input2,"Passwords do not match")
+}
+}
 
 // Event listeners
 form.addEventListener("submit", function (e) {
@@ -77,4 +100,8 @@ form.addEventListener("submit", function (e) {
   // }
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password,password2)
 });
